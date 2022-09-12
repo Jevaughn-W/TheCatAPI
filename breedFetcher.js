f0553aaba059d3c1e82b0f50c79e6a4778d54ffa
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2022-09-11 23:02:32 -0400

    refactor for modularity

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,35 @@
 const request = require('request');
-const breedToQuery = process.argv;
-breedToQuery.splice(0,2);
 
 const baseApiUrl = 'https://api.thecatapi.com/v1/breeds/search?q=';
 
 // console.log(breedToQuery[0]); // check that the arrray has been altered successfully
 
-request(baseApiUrl + breedToQuery[0], (err, response, body) => { // all three arguments are needed
-  if (err) {
-    console.log('Error', err);
-  } else {
-    // console.log('Status Code', response.statusCode); // Give the result of the http request
-    // console.log(typeof body); // Checking that the body is a JSON
-    const data = JSON.parse(body); // Converting from JSON to an object
-    // console.log( typeof data); // Checking that the conversion was succesful
-    if (data[0] === undefined) {
-      console.log('Cat breed not found, description is unavailable');
+const fetchBreedDescription = function(breedName, callback) {
+  
+  request(baseApiUrl + breedName, (err, response, body) => { // all three arguments are needed
+    let description;
+    let error;
+    if (err) {
+      error = err;
+      callback(error, description);
     } else {
-      console.log(data[0].description);
+      // console.log('Status Code', response.statusCode); // Give the result of the http request
+      // console.log(typeof body); // Checking that the body is a JSON
+      const data = JSON.parse(body); // Converting from JSON to an object
+      // console.log( typeof data); // Checking that the conversion was succesful
+      if (data[0] === undefined) {
+        // console.log('Cat breed not found, description is unavailable');
+        error = "Cat breed not found, description is unavailable.";
+        callback(error, description);
+      } else {
+        // console.log(data[0].description);
+        description = data[0].description;
+        callback(err, description);
+      }
     }
-  }
-});
+  });
+
+};
+
+
+module.exports = { fetchBreedDescription };
